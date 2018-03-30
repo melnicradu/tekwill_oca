7bbe5e48b8c0411ce94d969eeaff500b52426509
melnicradu <djinfectionmd@gmail.com>
2018-03-30 19:53:02 +0300

    lesson4`

---
+++ lesson4/src/ShopingCart.java
@@ -0,0 +1,11 @@
+public class ShopingCart {
+    public static void main(String[] args){
+        // Declare and initialize String variables. Do not initialize message yet.
+        String custName ="James Bond";
+        String intemDesc ="book";
+        String message;
+        message = custName + " need a  " + intemDesc;
+        System.out.print(message);
+
+    }
+}
